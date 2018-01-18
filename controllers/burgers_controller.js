05ace49c99cb0c780b76543d29531205fb0763e8
Paul Zavattieri <paulz92@gmail.com>
2018-01-18 12:25:29 -0500

    controllers, server code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,20 +4,39 @@ var burger = require("../models/burger.js");
 
 var router = express.Router();
 
-router.get("", function(req, res) {
-
+router.get("/", function(req, res) {
+	res.redirect("/burgers");
 });
 
-router.get("", function(req, res) {
-
+// get route for burgers
+router.get("/burgers", function(req, res) {
+	// using select all burger model to call mysql for getting all info
+	burger.selectAll(function(burgerInfo) {
+		// sends the callback info to index handlebars in an object with property burgers
+		res.render("index", {burgers: burgerInfo});
+	});
 });
 
-router.post("", function(req, res) {
-
+// post route for new burgers
+router.post("/burgers/new", function(req, res) {
+	// insert burger function from model with req body as burger name and call back 
+	// the data to log and redirect back home
+	burger.insertOne(req.body.burger_name, function(data) {
+		console.log(data);
+		res.redirect("/");
+	});
 });
 
-router.put("", function(req, res) {
-
+// put route for updated burger
+router.put("/burgers/:id", function(req, res) {
+	// update with req params id and call back with data based on model update method
+	burger.updateOne(req.params.id, function(data) {
+		// log data
+		console.log(data);
+		// send back success status
+		res.sendStatus(200);
+	});
 });
 
+// export the router
 module.exports = router;
