cdf2255bef7ef09004f5662eeb16b7699edbf8e5
Paul Zavattieri <paulz92@gmail.com>
2018-01-18 15:55:55 -0500

    app completed minus readme

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
--- pop table with 3 default burgers and display table
+-- populate table with 3 default burgers and display table
 INSERT INTO burgers (burger_name) VALUES ("cheeseburger");
 INSERT INTO burgers (burger_name) VALUES ("bacon cheeseburger");
 INSERT INTO burgers (burger_name) VALUES ("hamburger");
--- models/burger.js
+++ models/burger.js
@@ -1,17 +1,19 @@
 // require orm
 var orm = require("../config/orm.js");
 
-// create code here that will call orm functions using burger specific input for the
-// orm
+// code which calls orm functions but specific to the burgers
 var burger = {
+	// call the select all method
 	selectAll: function(cb) {
 		orm.selectAll(function(results) {
 			cb(results);
 		});
 	},
+	// call the insert method
 	insertOne: function(name, cb) {
 		orm.insertOne(name, cb);
 	},
+	// call the update method
 	updateOne: function(id, cb) {
 		orm.updateOne(id, cb);
 	}
